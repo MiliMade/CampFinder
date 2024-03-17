9e6abe1a3a80128b7061a45f03ac05926bac4e73
MiliMade <milimadeemail@gmail.com>
2024-03-17 10:33:00 -0400

    Installed ejs-mate and added boilerplate layout to views

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ import { fileURLToPath } from "node:url"
 import mongoose from "mongoose"
 import {Campground} from "./models/campground.js"
 import methodOverride from "method-override"
+import ejsMate from "ejs-mate"
 
 const app = express()
 const PORT = 8080
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
+ 
+app.engine('ejs', ejsMate)
+ 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
-
-async function main() {
+ 
+async function main() { 
   try{
      await mongoose.connect('mongodb://127.0.0.1:27017/camp-finder');
      console.log("connected to database")
