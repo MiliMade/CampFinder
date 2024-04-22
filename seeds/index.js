c566dfbb5ea4baa187d5408a0d1b74f4e11102e1
MiliMade <milimadeemail@gmail.com>
2024-04-21 20:15:04 -0400

    added image to shwo page

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,14 @@ const seedDB = async() =>{
   await Campground.deleteMany({})
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random()*1000)
+    const randomPrice = Math.floor(Math.random()*30) + 10
     const camp = 
     new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      title:`${sampleName(descriptors)} ${sampleName(places)}`
+      title:`${sampleName(descriptors)} ${sampleName(places)}`,
+      image:'https://source.unsplash.com/collection/9046579', 
+      description:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex incidunt eum laborum, nihil harum ratione atque totam repellat tempore facilis id quisquam, quod natus unde error nisi exercitationem! Minus, sapiente.',
+      price: randomPrice
     })
     await camp.save()
   }
